614611a2d67caac1f989d68fa36fec0bf6a7c0d8
Dan Reeves <djreeves1292@gmail.com>
2019-06-29 02:11:11 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+var list = ['cars','boats','planes'];
+var pets = ['cats','dogs','birds'];
+
+
+function addElementToBeginningOfArray(list,trains) {
+  return ['trains', ...list];
+}
+
+function destructivelyAddElementToBeginningOfArray(pets,hamsters) {
+  return pets.unshift('hamsters');
+}
+
+var names = ['Dan','Amber','Jelly'];
+var jobs = ['baker','clerk','janitor'];
+
+function addElementToEndOfArray(names,Isiah) {
+  return [...names,'Isiah'];
+}
+
+function destructivelyAddElementToEndOfArray(jobs,mailman) {
+  return jobs.push('mailman');
+}
+
+var sounds = ['vroom','skrrt'];
+
+function accessElementInArray(sounds) {
+  return sounds[1];
+}
